ecbea8906ac7678a05fd5e4bad91ad271d75d981
18727060063 <www.794430340@qq.com>
2019-11-08 20:31:45 +0800

    更新网页

--- DjangoApp/views.py
+++ DjangoApp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render_to_response
 # Create your views here.
+from DjangoApp import loadNormalData
 from DjangoApp.models import NormalPeople
 
 
 def index(request):
+    loadNormalData.readTecExcel()
     bb= NormalPeople.objects.all()
     return render_to_response('index.html', {'data': bb})
 
+def initData(request):
+    print("测试")
